8cd1971e66d7ec3c7ca2e3afc766e1a055d84e71
github username <github email address>
2018-02-14 19:26:12 -0500

    Added Classes and additional functionalility

--- app/models/favorites.rb
+++ app/models/favorites.rb
@@ -1,10 +1,12 @@
 class Favorites < ActiveRecord::Base
-  belongs_to: :users
-  belongs_to: :songs
-  has_many: :artists, through: :songs
-  has_many: :lyrics, through: :songs
+  belongs_to :users
+  belongs_to :songs
+  has_many :artists, through: :songs
+  has_many :lyrics, through: :songs
 
-
+  def self.save_song_to_favorites(songid, userid)
+    self.create(song_id: songid, user_id: userid)
+end
 
 
 end
